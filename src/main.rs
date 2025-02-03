9531b3cd1e7e73d24d58aaaa913c3b4176370614
Tim Fennis <fennis.tim@gmail.com>
2025-02-03 08:52:20 +0100

    Updated reproducer, SDL does show the field

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use async_graphql::{EmptyMutation, EmptySubscription, InputObject, Object, Schema};
+use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema};
 
 const QUERY: &'static str = r#"
 {
@@ -20,21 +20,21 @@ const QUERY: &'static str = r#"
 #[tokio::main]
 async fn main() {
     let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
+    // The deprecated field does show up here
+    println!("{}", schema.sdl());
+
     let res = schema.execute(QUERY).await;
     let out = res.data.into_json().unwrap();
+
+    // The deprecated field does not show up here
     println!("{out}");
 }
 
 struct Query;
 
-#[derive(InputObject)]
-struct Bar {
-    num: i32,
-}
-
 #[Object]
 impl Query {
-    async fn foo(&self, #[graphql(deprecation = "deprecated")] bar: Bar) -> i32 {
-        420
+    async fn foo(&self, #[graphql(deprecation = "deprecated")] bar: i32) -> i32 {
+        bar
     }
 }
